327b4725d9cda9184f9ff2d3e1cb762c9694b6f4
王derful <11409153+wang-derful@user.noreply.gitee.com>
2022-09-10 20:58:16 +0800

    2022/9/10

---
+++ src toRaw与markRaw/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- 模板里面会自动解析引用对象的value，即name.value -->
+  <button @click="isShow = !isShow">切换显示/隐藏</button>
+  <Demo v-if="isShow"> </Demo>
+</template>
+<script>
+import Demo from './components/Demo.vue';  
+import {ref} from 'vue'
+export default {
+    name: "App",
+    components: { Demo },
+    setup() {
+      let isShow = ref(true)
+      return {
+      isShow
+    }
+    },   
+}
+</script>
