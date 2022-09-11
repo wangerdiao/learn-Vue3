e8809b2afcb890a790561691dfa7b73d2c313722
王derful <11409153+wang-derful@user.noreply.gitee.com>
2022-09-11 14:47:01 +0800

    2022/9/11

---
+++ src teleport组件/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">
+    <h1>我是App组件</h1>
+    <Child></Child>
+  </div>
+</template>
+
+<script>
+import Child from "./components/Child";
+export default {
+    name: "App",
+    components: { Child },
+}
+</script>
+<style>
+  .app {
+    background-color: red;
+    padding: 10px;
+  }
+</style>
